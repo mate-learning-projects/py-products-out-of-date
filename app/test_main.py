03274604f009eb3e2261e3a48238b2b0854244ea
Oleksii Ivanov <oleks4335@gmail.com>
2024-10-27 07:43:02 +0100

    Add Products out of date solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,38 @@
-# write your code here
+import datetime
+from unittest import mock
+
+import pytest
+
+from app.main import outdated_products
+
+
+@pytest.mark.parametrize(
+    "mocked_date,result",
+    [
+        (datetime.date(2022, 2, 2), ["duck"]),
+        (datetime.date(2022, 1, 30), []),
+        (datetime.date(2022, 2, 5), ["duck"]),
+        (datetime.date(2022, 2, 11), ["salmon", "chicken", "duck"]),
+    ],
+)
+def test_outdated_products(mocked_date: mock, result: list) -> None:
+    products = [
+        {
+            "name": "salmon",
+            "expiration_date": datetime.date(2022, 2, 10),
+            "price": 600
+        },
+        {
+            "name": "chicken",
+            "expiration_date": datetime.date(2022, 2, 5),
+            "price": 120
+        },
+        {
+            "name": "duck",
+            "expiration_date": datetime.date(2022, 2, 1),
+            "price": 160
+        },
+    ]
+    with mock.patch("datetime.date") as mock_date:
+        mock_date.today.return_value = mocked_date
+        assert outdated_products(products) == result
